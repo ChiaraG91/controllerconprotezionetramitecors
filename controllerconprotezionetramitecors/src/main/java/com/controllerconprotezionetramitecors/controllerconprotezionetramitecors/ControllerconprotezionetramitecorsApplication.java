ef5c65d3e70ecfd74f0479d674590f3073e76235
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-20 16:25:40 +0100

    Add files via upload

---
+++ controllerconprotezionetramitecors/src/main/java/com/controllerconprotezionetramitecors/controllerconprotezionetramitecors/ControllerconprotezionetramitecorsApplication.java
@@ -0,0 +1,13 @@
+package com.controllerconprotezionetramitecors.controllerconprotezionetramitecors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerconprotezionetramitecorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerconprotezionetramitecorsApplication.class, args);
+	}
+
+}
---
+++ controllerconprotezionetramitecors/src/test/java/com/controllerconprotezionetramitecors/controllerconprotezionetramitecors/ControllerconprotezionetramitecorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controllerconprotezionetramitecors.controllerconprotezionetramitecors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerconprotezionetramitecorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
